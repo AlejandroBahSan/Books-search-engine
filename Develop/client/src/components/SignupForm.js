06e4997718856325ac74c77463f19fe155815c4f
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-08-05 18:57:12 -0500

    Apollo Ready

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client'
+import { CREATE_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +11,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [createUser, { error, data }] = useMutation(CREATE_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,6 +26,18 @@ const SignupForm = () => {
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
+    }
+
+    try {
+      const { data } = await createUser({
+        variables: userFormData
+      });
+
+      console.log(data);
+      Auth.login(data.createUser.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
     }
 
     try {
